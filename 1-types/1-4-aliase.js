012c67bd14db98045fd90c6bfcb9f626590f50a8
Seogun95 <ksj083895@gmail.com>
2023-05-06 21:29:08 +0900

    feat: aliase 추가

---
+++ 1-types/1-4-aliase.js
@@ -0,0 +1,15 @@
+{
+    var name_1 = '서근';
+    var address = '대전';
+    var resume = {
+        name: '김선중',
+        age: 28,
+        profession: 'FrontEnd Developer'
+    };
+    resume.profession = '프론트엔드 개발자';
+    console.log('김선중 이력서 --->', resume);
+    /* const seogun: Seogun = '선중'; */ // '"선중"' 형식은 '"서근"' 형식에 할당할 수 없습니다.
+    var seogun = '서근';
+    var isCurrent = true;
+    console.log(isCurrent);
+}
